5ea65872fa656fabadf825b4ebcc4cddac4d6405
Larry Castillo <larrycastillo2009@gmail.com>
2020-06-22 21:09:57 -0500

    1st commit

---
+++ src/Contact.java
@@ -0,0 +1,24 @@
+public class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Contact(String name) {
+        this.name = name;
+    }
+}
